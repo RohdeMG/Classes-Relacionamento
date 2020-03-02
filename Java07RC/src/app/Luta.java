97452dcd903829127e796dbf2a2f63a6542c56ab
RohdeMG <rohde1392@gmail.com>
2020-03-02 15:48:19 +0000

    ij

--- Java07RC/src/app/Luta.java
+++ Java07RC/src/app/Luta.java
@@ -1,1 +1,90 @@
 package app;
+//CLASSE QUE AGREGA outra classe
+
+import java.util.Random;
+
+//um objeto tem outro objeto (AGREGAÇÃO ^) ligação de losango
+//                                      v
+
+public class Luta {
+    private Lutador desafiante;
+    private Lutador desafiado;
+    private int rounds;
+    private boolean aprovada;
+
+    public void marcarLuta(Lutador l1, Lutador l2) {
+        if (l1.getCategoria() == l2.getCategoria() && l1 != l2) {
+            this.aprovada = true;
+            desafiante = l2;
+            desafiado = l1;
+        } else {
+            this.aprovada = false;
+            desafiante = null;
+            desafiado = null;
+        }
+    }
+
+    public void lutar() {
+        if(this.getAprovada()){
+            System.out.println("### VS ###");
+            this.desafiado.apresentar();
+            this.desafiante.apresentar();
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); // aqui gera 3 resultados (0,1 ou 2) 
+            System.out.println("O resultado é:");
+            switch(vencedor){
+                case 0: //Empate
+                    System.out.println("*Empate*");
+                    desafiado.empatarLuta();
+                    desafiante.empatarLuta();
+                    break;
+                case 1: //Vitória do desafiado
+                    System.out.println(desafiado.getNome() +" *Ganhou*");
+                    desafiado.ganharLuta();
+                    desafiante.perderLuta();
+                    break;
+                case 2: //Derrota do desafiante
+                    System.out.println(desafiante.getNome() +" *Ganhou*");
+                    desafiante.ganharLuta();
+                    desafiado.perderLuta();
+                    break;
+            }   
+            System.out.println("\n\n");
+        }else{
+            System.out.println("Essa luta não pode acontecer!");
+        }
+    }
+
+    // métodos Especiais
+    public Lutador getDesafiante() {
+        return this.desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public Lutador getDesafiado() {
+        return this.desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public int getRounds() {
+        return this.rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean getAprovada() {
+        return this.aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+}
--- Java07RC/src/app/Principal.java
+++ Java07RC/src/app/Principal.java
@@ -2,10 +2,15 @@ package app;
 
 //Relacionamento entre classes
 
+//=====================================================
+// ****** OBJETOS SÃO ISTÂNCIAS DE CLASSES ***********
+//=====================================================
+
+
 public class Principal {
     public static void main(String[] args) throws Exception {
+        //Como são vários lutadores, utilizamos um VETOR
         Lutador l[] = new Lutador[6];
-
         l[0] = new Lutador("Pretty Boy","França", 31, 1.75f, 68.9f, 11, 2, 1);
         l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
         l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
@@ -14,11 +19,12 @@ public class Principal {
         l[5] = new Lutador("Nerdart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
 
 
-
-        l[3].apresentar();
-        l[3].status();
-
-        //Como são vários lutadores, utilizamos um VETOR
+       Luta UEC01 = new Luta();
+       UEC01.marcarLuta(l[4], l[1]);
+       UEC01.lutar();
+    //    l[4].status();
+    //    l[5].status();
+        
     }
 
 }
